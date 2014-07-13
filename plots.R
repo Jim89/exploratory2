325e65d68642d5069976a94294da91d76da63660
Jim89 <jimmy22theave@googlemail.com>
2014-07-13 11:05:54 +0100

    Updated with plot2 code

--- plots.R
+++ plots.R
@@ -3,39 +3,54 @@
 # Authour: Jim Leach
 # Data: 2014/07/13
 
-dir<-"/home/jim/R/exploratory_analysis/project2"
+dir<-"/home/jim/R/exploratory_analysis/project2/exploratory2"
 setwd(dir)
 ################################################################################
-# Step 1: Load required packages
+# Load required packages
 ################################################################################
 require(dplyr,quietly=T)
 require(lubridate,quietly=T)
 
 ################################################################################
-# Step 2: Check if data exists, if not download and unzip. Then read in the data
+# Check if data exists, if not download and unzip. Then read in the data
 ################################################################################
 url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
 
-if(!file.exists(c("summarySCC_PM25.rds","Source_Classification_Code.rds")))
+if(!file.exists("./data/summarySCC_PM25.rds")|!file.exists("./data/Source_Classification_Code.rds"))
   {
-  download.file(url,"data.zip",method="curl",quiet=T)
-  unzip("data.zip",overwrite=TRUE)
+  download.file(url,"./data/data.zip",method="curl",quiet=T)
+  unzip("./data/data.zip",overwrite=TRUE,exdir="./data")
   }
-
-data<-readRDS("summarySCC_PM25.rds")
-sources<-readRDS("Source_Classification_Code.rds")
-    
+data<-readRDS("./data/summarySCC_PM25.rds")
+sources<-readRDS("./data/Source_Classification_Code.rds")    
 
 ################################################################################
-# Step 3: Plot 1 - total PM2.5 Emissions Per Year
+# Plot 1 - total PM2.5 Emissions Per Year - all regions
 ################################################################################
-summary<-summarize(group_by(select(data,Emissions,year),year),total=sum(Emissions))
+all_regions_summary<-summarize(group_by(select(data,Emissions,year),year),total=sum(Emissions))
 options(scipen=10)
 png("plot1.png")
-barplot(summary$total,
+barplot(all_regions_summary$total,
         col="darkblue",
         xlab="Year",
-        ylab=expression('Total Emissions (tons) PM'[2.5]),
-        names.arg=summary$year
+        ylab=expression('Tons PM'[2.5]),
+        main=expression("Total Emissions PM"[2.5]),
+        names.arg=all_regions_summary$year
         )
+dev.off()
+
+################################################################################
+# Plot 2 - total PM2.5 Emissions Per Year - Baltimore, MD only
+################################################################################
+baltimore <-filter(data,data$fips=="24510")
+balt_summary<-summarize(group_by(select(baltimore,Emissions,year),year),total=sum(Emissions))
+rm(baltimore)
+png("plot2.png")
+barplot(balt_summary$total,
+        col="darkblue",
+        xlab="Year",
+        ylab=expression('Tons PM'[2.5]),
+        main=expression("Baltimore, MD: Emissions PM"[2.5]),
+        names.arg=balt_summary$year        
+)
 dev.off()
